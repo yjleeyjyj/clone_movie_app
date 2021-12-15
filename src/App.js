5b672f9f33b4063cef53901b7ca78e20779ca34f
SecondMusk_SecondGun <yjleeyjyj@gmail.com>
2021-12-15 15:39:04 +0900

    04-3. key prop 추가하기

--- src/App.js
+++ src/App.js
@@ -11,37 +11,38 @@ function Food({ name, image }) {
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://www.seriouseats.com/thmb/mANmBYm-pJfwrwHksChVWur6w9w=/1500x1125/filters:fill(auto,1)/20210527-baechu-kimchi-vicky-wasik-seriouseats-seriouseats-3-18a2d6d7d1d74a7a82cb13ed350218be.jpg',
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image: 'http://www.gildedgingerbread.com/wp-content/uploads/2017/08/Samgyeopsal-1.jpg',
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://cf.creatrip.com/original/blog/8937/166lv4gz4u0ei0nv8oykcq5t61w2zdtf.jpg',
   },
   {
+    id: 4,
     name: 'Doncasu',
     image: 'http://gdimg.gmarket.co.kr/2095588051/still/600?ver=1624507986',
   },
   {
+    id: 5,
     name: 'Kimbab',
     image: 'https://lh3.googleusercontent.com/proxy/rxPsPGZaTjXd5C_OOZ4egP5LDhLGfrRSyjCSnNTm3pbNZIqn7jBFC-ygmTNn-FE1pZkleZ7M9KMc7b3g6mgkxxoTmOQ5aTcHRO8Qqw9fr4w6xOkDCxE',
   },
 ];
 
-function renderFood(dish) {
-  return <Food name={dish.name} image={dish.image} />;
-}
-
-// const renderFood = dish => <Food name={dish.name} image={dish.image} />;
-
 function App() {
   return (
   <div>
-    { foodILike.map(renderFood)}
+    {foodILike.map(dish => (
+      <Food id={dish.id} name={dish.name} image={dish.image} />
+    ))}
   </div>
   );
     // <div className="App" />;
