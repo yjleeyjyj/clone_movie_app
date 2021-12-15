73607d6dca3245b83bbe2fc7a1c8d4491d4ecff2
SecondMusk_SecondGun <yjleeyjyj@gmail.com>
2021-12-15 14:00:21 +0900

    03-3. 리액트 기초 개념 1 & 2, Component, JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
+
+function Potato() {
+  return (
+    <h3>I love Potato</h3>
+  );
+}
 
 function App() {
   return (
   <div>
     <h1>Hello</h1>
+    <Potato />
   </div>
   );
     // <div className="App" />;
